1468db3645b82d340d5aa2f94def6f73a939e77d
Justinas Petrulis <justinas.petrulis@gmail.com>
2020-10-21 18:26:23 +0300

    Initial test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import yargs from 'yargs';
+import fs from 'promise-fs'
+import SSM from 'aws-sdk/clients/ssm';
 // main is main function which is started by the runner during
 // step execution.
 async function main(args: any) {
-  console.log(process.env)
+  const ssm: SSM = new SSM({ region: 'eu-west-1' });
+  const options = {
+    Name: '/builds/secrets/token', /* required */
+    WithDecryption: true
+  }
+  try {
+    const data = await ssm.getParameter(options).promise()
+    const secrets: any = {}
+    secrets.TOKEN = data.Parameter?.Value
+    await fs.writeFile("./secrets.json", JSON.stringify(secrets))
+  } catch {
+    console.log("error: unable to decrypt parameter")
+    process.exit(1)
+  }
 }
 // start main
 main(yargs(process.argv).argv)
