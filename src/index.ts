a9a697fa9938a0b4fa777e9761c156a34653bb84
Justinas Petrulis <justinas.petrulis@gmail.com>
2020-10-27 12:40:52 +0200

    Save secrets to output dir

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import fs from 'promise-fs';
-import path from 'path';
+import _path from 'path';
 import SSM from 'aws-sdk/clients/ssm';
+
+const outputDir = process.env.NEX_STEP_OUTPUT_DIR || __dirname;
 
 // Event represents extension event.
 type Event = {
@@ -14,16 +16,13 @@ type Event = {
 // main is main function which is started by the runner during
 // step execution.
 export async function run(event: Event) {
-  const env = process.env
-  const parameters = event.parameters;
-  const workspace = env.NEX_WORKSPACE
-  if (!workspace || workspace === "") {
-    return new Error("workspace was not set")
-  }
-  const ssm: SSM = new SSM({ region: parameters.region || 'eu-west-1' })
+  // Deconstruct parameters.
+  const { region, decrypt, path } = event.parameters;
+  // Setup ssm and prepare parameters.
+  const ssm: SSM = new SSM({ region: region || 'eu-west-1' })
   const params = {
-    decrypt: event.parameters.decrypt,
-    path: event.parameters.path || './secrets.json',
+    decrypt,
+    path: path || './secrets.json',
   }
   try {
     const secrets = await params.decrypt.reduce(async (memo, name: string) => {
@@ -39,9 +38,10 @@ export async function run(event: Event) {
       // Set secret.
       return { ...(await memo), [name]: data.Parameter?.Value }
     }, {})
-    const filepath = path.resolve(path.join(workspace, params.path))
+    const filepath = _path.resolve(_path.join(outputDir, params.path))
+    console.log(filepath)
     // By default it's path to workspace
-    const dir = path.dirname(filepath)
+    const dir = _path.dirname(filepath)
     if (!fs.existsSync(dir)) {
       // Create directory.
       await fs.mkdir(dir)
